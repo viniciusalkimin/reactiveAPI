a89c428d1c787e2c519eb8553da3562a6e56048f
viniciusalkimin <viniciusg_alkimin@hotmail.com>
2023-08-30 16:40:46 -0300

    Creation of base project

---
+++ src/main/java/com/api/webflux/reactiveAPI/ReactiveApiApplication.java
@@ -0,0 +1,13 @@
+package com.api.webflux.reactiveAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/webflux/reactiveAPI/ReactiveApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.webflux.reactiveAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
